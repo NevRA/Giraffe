59b82d358533540a991107cade82ce4136c385ee
Roman Abdulmanov <roman.abdulmanov@gmail.com>
2013-08-27 23:52:58 +0400

    Moved to maven

--- app/src/main/java/com/home/giraffe/Main.java
+++ app/src/main/java/com/home/giraffe/Main.java
@@ -1,15 +1,14 @@
 package com.home.giraffe;
 
-import android.app.Activity;
 import android.os.Bundle;
+import com.github.rtyley.android.sherlock.roboguice.activity.RoboSherlockActivity;
 
-public class Main extends Activity {
+public class Main extends RoboSherlockActivity {
     /**
      * Called when the activity is first created.
      */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
     }
 }
