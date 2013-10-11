a6de4bd8302837f4261836932db439d20b4752a5
NevRA <roman.abdulmanov@gmail.com>
2013-10-11 19:21:44 +0400

    Added comment views to post view

--- app/src/main/java/com/home/giraffe/ui/PostFragment.java
+++ app/src/main/java/com/home/giraffe/ui/PostFragment.java
@@ -5,10 +5,16 @@ import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
 import android.view.View;
 import android.webkit.WebView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import com.github.rtyley.android.sherlock.roboguice.activity.RoboSherlockFragmentActivity;
 import com.google.inject.Inject;
 import com.home.giraffe.Constants;
 import com.home.giraffe.R;
+import com.home.giraffe.interfaces.IImageLoader;
+import com.home.giraffe.objects.Actor;
+import com.home.giraffe.objects.Comment;
 import com.home.giraffe.objects.Post;
 import com.home.giraffe.storages.ObjectsStorage;
 import com.home.giraffe.tasks.GetPostTask;
@@ -17,6 +23,9 @@ import roboguice.inject.InjectView;
 public class PostFragment extends RoboSherlockFragmentActivity implements LoaderManager.LoaderCallbacks<Post> {
     private String mId;
     private Post mPost;
+
+    @Inject
+    IImageLoader mImageLoader;
 
     @Inject
     ObjectsStorage mObjectsStorage;
@@ -28,7 +37,7 @@ public class PostFragment extends RoboSherlockFragmentActivity implements Loader
     WebView mPostView;
 
     @InjectView(R.id.comments)
-    View mCommentsView;
+    LinearLayout mCommentsView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,7 +66,22 @@ public class PostFragment extends RoboSherlockFragmentActivity implements Loader
     }
 
     private void addComments() {
+         for (Comment comment : mPost.getComments()){
+             View commentView = getLayoutInflater().inflate(R.layout.comment, mCommentsView, false);
 
+             Actor actor = comment.getActor();
+             ImageView avatar = (ImageView) commentView.findViewById(R.id.avatar);
+             String avatarUrl = actor.getAvatarUrl();
+             mImageLoader.DisplayImage(avatarUrl, avatar);
+
+             TextView userDisplayName = (TextView) commentView.findViewById(R.id.userDisplayName);
+             userDisplayName.setText(actor.getDisplayName());
+
+             WebView content = (WebView) commentView.findViewById(R.id.content);
+             content.loadDataWithBaseURL("", "<body style=\"margin: 0; padding: 0\">" + comment.getRawContent(), "text/html", "UTF-8", null);
+
+             mCommentsView.addView(commentView);
+         }
     }
 
     @Override
